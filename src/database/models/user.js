77a54430c7d09d84518e76c5af80878f1dc0f648
Marcela Arroyo <malvesarroyo@gmail.com>
2022-06-20 16:35:45 -0300

    feat: creating User table migrations

---
+++ src/database/models/user.js
@@ -0,0 +1,12 @@
+const UserSchema = (sequelize, DataTypes) => {
+  const UserTable = sequelize.define('User', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  });
+
+  return UserTable;
+}
+
+module.exports = UserSchema;
