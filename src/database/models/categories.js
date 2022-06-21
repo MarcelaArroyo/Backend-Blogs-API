547814a27dd6c2e7365baf83b8ed95e7864f9b50
Marcela Arroyo <malvesarroyo@gmail.com>
2022-06-21 09:40:41 -0300

    feat: creating the model the category

--- src/database/models/categories.js
+++
@@ -1,9 +0,0 @@
-const CategoriesSchema = (sequelize, DataTypes) => {
-  const CategoriesTable = sequelize.define('Categories', {
-    name: DataTypes.STRING
-  });
-
-  return CategoriesTable;
-};
-
-module.exports = CategoriesSchema;
