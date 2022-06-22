ba1a140429e49b13b9135d01390dbc2683f31d58
Marcela Arroyo <malvesarroyo@gmail.com>
2022-06-22 11:16:54 -0300

    feat: creating the application with the GET /categories endpoint

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -5,11 +5,18 @@ const categoriesRouter = express.Router();
 const { tokenValidation } = require('../middlewares/token.middleware');
 const { categoriesValidation } = require('../middlewares/categories.middleware');
 
-const { createCategory } = require('../services/categories.service');
+const { createCategory,
+  getAllCategories,
+} = require('../services/categories.service');
 
 categoriesRouter.post('/', tokenValidation, categoriesValidation, async (req, res) => {
   const category = await createCategory(req.body);
   res.status(201).json(category);
+});
+
+categoriesRouter.get('/', tokenValidation, async (req, res) => {
+  const categories = await getAllCategories();
+  res.status(200).json(categories);
 });
 
 module.exports = categoriesRouter;
--- src/services/categories.service.js
+++ src/services/categories.service.js
@@ -2,6 +2,11 @@ const { Category } = require('../database/models');
 
 const createCategory = ({ name }) => Category.create({ name });
 
+const getAllCategories = () => Category.findAll({
+  attributes: { exclude: ['createdAt', 'updatedAt'] },
+});
+
 module.exports = {
   createCategory,
+  getAllCategories,
 };
