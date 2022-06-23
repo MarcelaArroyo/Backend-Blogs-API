6bf435356e162dfd048c43040accc0974b9fc156
Marcela Arroyo <malvesarroyo@gmail.com>
2022-06-23 14:20:11 -0300

    feat: creating the application with the GET /post endpoint

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -5,7 +5,7 @@ const postRouter = express.Router();
 const { tokenValidation } = require('../middlewares/token.middleware');
 const { postValidation } = require('../middlewares/post.middleware');
 
-const { createPost } = require('../services/post.service');
+const { createPost, getAllPosts } = require('../services/post.service');
 
 postRouter.post('/', tokenValidation, postValidation, async (req, res) => {
   const user = res.locals.payload;
@@ -14,6 +14,11 @@ postRouter.post('/', tokenValidation, postValidation, async (req, res) => {
   if (post.errMessage) return res.status(400).json({ message: post.errMessage });
 
   res.status(201).json(post);
+});
+
+postRouter.get('/', tokenValidation, async (req, res) => {
+  const posts = await getAllPosts();
+  res.status(200).json(posts);
 });
 
 module.exports = postRouter;
--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,4 +1,4 @@
-const { BlogPost, Category, PostCategory } = require('../database/models');
+const { BlogPost, Category, PostCategory, User } = require('../database/models');
 
 const createPostCategory = (postId, categoryIds) => categoryIds.forEach((category) => {
   PostCategory.create({
@@ -24,6 +24,20 @@ const createPost = async ({ title, content, categoryIds }, userId) => {
   return post;
 };
 
+const getAllPosts = () => BlogPost.findAll({
+    include: [{
+      model: User,
+      as: 'user',
+      attributes: { exclude: ['password', 'createdAt', 'updatedAt'] },
+    },
+    { model: Category,
+      as: 'categories',
+      attributes: { exclude: ['createdAt', 'updatedAt'] },
+    }],
+    attributes: { exclude: ['createdAt', 'updatedAt'] },
+  });
+
 module.exports = {
   createPost,
+  getAllPosts,
 };
